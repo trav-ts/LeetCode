1b1136132e7c55ec5b5109f29f9079c702e4b96c
trav-ts <travis.slade@outlook.com>
2022-11-06 16:15:14 -0700

    Finished daily LeetCode #899 OrderlyQueue

---
+++ LeetCode/LeetCodeDaily/OrderlyQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LeetCodeDaily
+{
+    /// <summary>
+    /// Daily problem for November 6th, 2022
+    /// 
+    /// #899 Orderly Queue
+    /// </summary>
+    public class OrderlyQueue
+    {
+        /// <summary>
+        /// You are given a string s and an integer k. You can choose one of the 
+        /// first k letters of s and append it at the end of the string..
+        /// 
+        /// Return the lexicographically smallest string you could have 
+        /// after applying the mentioned step any number of moves.
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public string SmallestString(string s, int k)
+        {
+            if(k >= 2)
+            {
+                char[] c = s.ToArray();
+                Array.Sort(c);
+                string newS = new string(c);
+                return newS;
+            }
+
+            var n = s.Length;
+            var best = s;
+            string current = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                current = s.Substring(i, n - i) + s.Substring(0, i);
+                if (string.Compare(current, best) < 0)
+                    best = current;
+            }
+            return best;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LeetCodeTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCodeTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3c667f9c-2165-45a8-aa17-fe8eaf61e9f2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
